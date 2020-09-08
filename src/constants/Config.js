0bbe0987d0dd0ccbbb1a9a7a9063c16f311280bc
Navu <44286578+navuthily@users.noreply.github.com>
2020-09-08 21:46:13 +0700

    Update Config.js

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,5 +1,6 @@
 //export const API_URL = "http://localhost:4000";
-const API_URL = "http://localhost:4000";
+//const API_URL = "http://localhost:4000";
+const API_URL="https://api-test-react-codersx.herokuapp.com/";
 const user = localStorage.getItem("user");
 module.exports = {
   API_URL,
